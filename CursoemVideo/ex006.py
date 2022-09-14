a5b0e8fe0b3654f146007a3ea958fb158a5a9aaa
Ronaldo Amorim <ronaldop.amorim@hotmail.com>
2022-09-13 23:59:24 -0300

    Exercício 006
    Programa para pegar um determinado número e escrever o seu dobro, triplo e raiz quadrada com até duas casas decimais.

---
+++ CursoemVideo/ex006.py
@@ -0,0 +1,7 @@
+numero = int(input('Digite um valor: '))
+dob = numero * 2
+tri = numero * 3
+rquad = numero**(1/2)
+print(f'O dobro de {numero} vale {dob}.\n'
+      f'O triplo de {numero} vale {tri}.\n'
+      f'A raíz quadrada de {numero} vale {rquad:.2f}.')
