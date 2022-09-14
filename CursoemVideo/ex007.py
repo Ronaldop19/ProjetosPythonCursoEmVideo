ea625d18ca6d2c29d6b9a063c11faa8056e56f75
Ronaldo Amorim <ronaldop.amorim@hotmail.com>
2022-09-14 00:53:24 -0300

    exercício 007
    programa básico que calcula a média aritmética de um aluno até 1 casa decimal

---
+++ CursoemVideo/ex007.py
@@ -0,0 +1,5 @@
+aluno = input('Digite o nome do aluno: ')
+nota1 = float(input('Digite a primeira nota do aluno: '))
+nota2 = float(input('Digite a segunda nota do aluno: '))
+m = (nota1+nota2)/2
+print(f'A média aritmética das notas de {aluno} é de {m:.1f}')
