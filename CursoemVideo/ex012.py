163901a399808b91f5afdc39a32fd12fd338babf
Ronaldo Amorim <ronaldop.amorim@hotmail.com>
2022-09-14 23:51:43 -0300

    ex012
    Programa que calcula o novo valor de um produto, com a porcentagem de desconto que você deseja oferecer.

---
+++ CursoemVideo/ex012.py
@@ -0,0 +1,5 @@
+prod = float(input('Digite o preço atual do produto: R$'))
+desc = int(input('Quantos % de desconto você gostaria de oferecer? '))
+desc = desc/100
+proddesc = prod - (prod * desc)      #produto com desconto
+print(f'O novo valor do produto com {desc*100}% de desconto é de R${proddesc:.2f}')
