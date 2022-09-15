fd570160fab396840d5373ebaa2b70ebbea97739
Ronaldo Amorim <ronaldop.amorim@hotmail.com>
2022-09-15 00:36:21 -0300

    ex014
    Programa para converter uma determinada temperatura em Celsius para Fahrenheit e Kelvin.

---
+++ CursoemVideo/ex014.py
@@ -0,0 +1,5 @@
+c = float(input('Digite a temperatura em Celsius: '))
+f = (c * 1.8) + 32
+k = c + 273.15
+print(f'A temperatura em Fahrenheit é de {f:.2f}Fº')
+print(f'A temperatura em Kelvin é de {k:.2f}K')
