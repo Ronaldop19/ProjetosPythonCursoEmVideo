6e6110ee2f7ec94dd45d5bcc48e6d9fbdf10557d
Ronaldo Amorim <110358540+Ronaldop19@users.noreply.github.com>
2022-09-14 23:58:55 -0300

    update ex012
    Modificação para que a porcentagem na mensagem final, apareça como um número inteiro.

--- CursoemVideo/ex012.py
+++ CursoemVideo/ex012.py
@@ -2,4 +2,4 @@ prod = float(input('Digite o preço atual do produto: R$'))
 desc = int(input('Quantos % de desconto você gostaria de oferecer? '))
 desc = desc/100
 proddesc = prod - (prod * desc)      #produto com desconto
-print(f'O novo valor do produto com {desc*100}% de desconto é de R${proddesc:.2f}')
+print(f'O novo valor do produto com {desc*100:.0f}% de desconto é de R${proddesc:.2f}')
