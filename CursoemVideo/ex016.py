0068ff7f337da70bfed75dc65be95ccf8a68e2f8
Ronaldo Amorim <ronaldop.amorim@hotmail.com>
2022-09-20 22:35:59 -0300

    ex016
    Programa para ler um número real qualquer e através da utilização de  um módulo, apresentar a parte inteira do número digitado.

---
+++ CursoemVideo/ex016.py
@@ -0,0 +1,4 @@
+import math
+num = float(input('Digite um valor real: '))
+print(f'O valor digitado foi {num} e sua parte inteira é {math.trunc(num)}')
+                                                         #ou utilize  o "int(num)"
